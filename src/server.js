30515b6ecffa30901b2e2c9f4fc6fee92aba3954
Sneha Verma <connectwither.sneha@gmail.com>
2026-09-18 15:49:50 +0530

    test PR checks workflow

--- src/server.js
+++ src/server.js
@@ -3,3 +3,4 @@ const app = express();
 app.get('/health', (req, res) => res.json({ status: 'healthy', version: process.env.IMAGE_TAG || 'local' }));
 app.get('/', (req, res) => res.send('RetailEdge API is running'));
 app.listen(3000, () => console.log('Listening on port 3000'));
+app.get('/', (req, res) => res.send('RetailEdge API is running - v2'));
