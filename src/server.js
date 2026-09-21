dcc86d5eea41d4c96d3dfd0bbdcac69b57e04a6f
sneha10-v <connectwither.sneha@gmail.com>
2026-09-21 20:26:09 +0530

    test full pipeline (#2)

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 app.get('/health', (req, res) => res.json({ status: 'healthy', version: process.env.IMAGE_TAG || 'local' }));
-app.get('/', (req, res) => res.send('RetailEdge API is running'));
+app.get('/', (req, res) => res.send('RetailEdge API is running - full pipeline test'));
 app.listen(3000, () => console.log('Listening on port 3000'));
